bffbc049381b776956623bcc409b5eef72f6a9cf
Andrew <ajwippl@gmail.com>
2016-03-19 17:49:43 -0400

    signin2

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,3 +1,8 @@
 /**
  * Created by awippl on 3/19/2016.
  */
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope) {
+    $scope.signing = function(username, password) {
+        console.log("I'm not done yet")
+    }
+});
